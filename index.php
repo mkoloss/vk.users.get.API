8c0512cb2b6947fb2f0bcee3988396392a15b151
mkoloss <kolosenkov@outlook.com>
2014-05-06 20:26:02 +0400

    VK api test already work!

--- index.php
+++ index.php
@@ -1,10 +1,32 @@
 <?php
-	$arr = array('a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>5,);
-	var_dump($arr);
+	$vkid = isset($_GET['vkid']) ? $_GET['vkid']:'1';
+	$vkarr = file_get_contents('https://api.vk.com/method/users.get?v=5.21&user_ids='.$vkid.'&fields=photo_200,city');
+	echo $vkarr.'<br>';
+	var_dump(json_decode($vkarr, true));
+	$new_arr = json_decode($vkarr, true);
 	echo '<br>';
-	$json = json_encode($arr);
-	echo $json;
-	echo '<br>';	
-	var_dump(json_decode($json));
-	echo '<br>';
-	var_dump(json_decode($json, true));
+	echo $new_arr['response'][0]['id'];
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+	<h1>Echo Vk Info</h1>
+	<form action="" method="get">
+		<input type="text" placeholder="Введите id" name="vkid">
+		<input type="submit" value="Получить даннные  пользователя">
+	</form>
+
+	<?php 
+		echo $new_arr['response'][0]['first_name'].' '.$new_arr['response'][0]['last_name'].'<br>';
+ 		echo $new_arr['response'][0]['city']['title'].'<br>';
+ 		echo '<img src="'.$new_arr['response'][0]['photo_200'].'">';
+ 	?>
+</body>
+</html>
